ea45339c712b1f768ae7e41743e5188dac2f88fd
Bernardo <bernardo930321@gmail.com>
2022-08-16 05:21:27 -0700

    script

--- index.js
+++ index.js
@@ -36,10 +36,14 @@ const server = new ApolloServer({
   },
 });
 
-// Run server
-server.listen({ port: 59313 || 4000 }).then(({ url }) => {
-  console.log(`Servidor listo en la URL ${url}`);
-});
+// // Run server
+// server.listen({ port: 59313 || 4000 }).then(({ url }) => {
+//   console.log(`Servidor listo en la URL ${url}`);
+// });
 
+const app = server.listen(process.env.PORT || 5000, () => {
+  const port = app.address().port;
+  console.log(`Express is working on port ${port}`);
+});
 // let port_number = server.listen(process.env.PORT || 4000);
 // server.listen(port_number);
