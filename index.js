8b2ec5bba1e316416fb0562172aa93366113365d
Bernardo <bernardo930321@gmail.com>
2022-08-17 08:27:15 -0700

    make it better

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const { ApolloServer } = require("apollo-server-express");
 const mongoose = require("mongoose");
-const express = require('express');
+const express = require("express");
 const resolvers = require("./db/resolvers");
 const typeDefs = require("./db/schema");
 const jwt = require("jsonwebtoken");
 const { graphqlUploadExpress } = require("graphql-upload");
 require("dotenv").config({ path: "variables.env" });
+const MongoClient = require("mongodb").MongoClient;
 
 // Conection to mongo DB
-mongoose.connect(
-  process.env.DB_MONGO,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err, _) => {
 
-    if(err) {
-      console.log('conecion failed');
-    } else {
-      server();
-    }
-  }
-);
+const client = await new MongoClient(process.env.DB_MONGO, {
+  useNewUrlParser: true,
+});
+client.connect();
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("MongoDB connection error: ", err);
+});
+
+server();
 
 // Server
-async function server(){
+async function server() {
   const serverApollo = new ApolloServer({
     typeDefs,
     resolvers,
@@ -45,16 +44,16 @@ async function server(){
         } catch (error) {
           console.log("Hubo un error");
           console.log(error);
-          throw new Error("token invalido")
+          throw new Error("token invalido");
         }
       }
     },
   });
   await serverApollo.start();
   const app = express();
-  app.use(graphqlUploadExpress())
+  app.use(graphqlUploadExpress());
   serverApollo.applyMiddleware({ app });
-  await new Promise((r) => app.listen({port: process.env.PORT || 5225}, r));
+  await new Promise((r) => app.listen({ port: process.env.PORT || 5225 }, r));
 
   console.log(`Servidor listo en la URL ${serverApollo.graphqlPath}`);
 }
