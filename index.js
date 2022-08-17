57d408b50535229145be56985f7f09d54086ba26
Bernardo <bernardo930321@gmail.com>
2022-08-17 10:43:19 -0700

    make it better

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function server() {
   const app = express();
   app.use(graphqlUploadExpress());
   serverApollo.applyMiddleware({ app });
-  await new Promise((r) => app.listen({ port: process.env.PORT || PORT }, r));
+  await new Promise((r) => app.listen({ port: process.env.PORT || 4000 }, r));
 
   console.log(`Servidor listo en la URL ${serverApollo.graphqlPath}`);
 }
